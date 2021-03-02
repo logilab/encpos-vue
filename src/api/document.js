17ccbdab602007a37706ae2e8a0008024cdf304f
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2021-03-02 12:15:26 +0100

    add .env

--- src/api/document.js
+++ src/api/document.js
@@ -1,17 +1,20 @@
+const _baseApiURL = `${process.env.VUE_APP_API_URL}`;
+
+
 async function getMetadataFromApi(id) {
-    const response_metadata = await fetch(`http://localhost:5050/dts/collections?id=${id}`, {mode: 'cors'})
+    const response_metadata = await fetch(`${_baseApiURL}/collections?id=${id}`, {mode: 'cors'})
     const metadata = await response_metadata.json()
     return metadata
 }
 
 async function getDocumentFromApi(id) {
-    const response = await fetch(`http://localhost:5050/dts/document?id=${id}&xslt`, {mode: 'cors'})
+    const response = await fetch(`${_baseApiURL}/document?id=${id}&xslt`, {mode: 'cors'})
     const document = await response.text()
     return document
 }
 
 async function getPositionAnneeFromApi(id) {
-    const response = await fetch(`http://localhost:5050/dts/collections?id=ENCPOS_${id}`, {mode: 'cors'})
+    const response = await fetch(`${_baseApiURL}/collections?id=ENCPOS_${id}`, {mode: 'cors'})
     const document = await response.json()
     return document
 }
