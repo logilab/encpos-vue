f0941b47357dfc99b313863f56a98af7abb02514
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2021-04-09 14:43:13 +0200

    add search agg function

--- src/api/elasticsearch.js
+++ src/api/elasticsearch.js
@@ -2,7 +2,6 @@ const _baseApiURL = `${process.env.VUE_APP_ELASTICSEARCH_URL}`;
 
 
 async function searchDocument(query, sorts, ranges, pageNum=1, pageSize=200) {
-    //TODO: implement 'group by' parameter
     let rangesArg = ''
     for (let range of ranges){
         rangesArg += `&range[${range.field}]=${range.ops}`
@@ -16,7 +15,27 @@ async function searchDocument(query, sorts, ranges, pageNum=1, pageSize=200) {
     return await response.json()
 }
 
+async function searchDocumentWithAgg(query, sorts, ranges, field, afterKey=null, pageSize=200) {
+    let rangesArg = ''
+    for (let range of ranges){
+        rangesArg += `&range[${range.field}]=${range.ops}`
+    }
+
+    let sortArg = ''
+    if (sorts) {
+        sortArg = '&sort=' + sorts
+    }
+    
+    let groupbyArg = `&groupby[field]=${field}`
+    if (afterKey) {
+        groupbyArg += `&groupby[after-page]=${afterKey}`
+    }
+
+    const response = await fetch(`${_baseApiURL}/search?query=${query}${sortArg}${rangesArg}${groupbyArg}&page[size]=${pageSize}`, {mode: 'cors'})
+    return await response.json()
+}
 
 export {
-    searchDocument
+    searchDocument,
+    searchDocumentWithAgg
 }
